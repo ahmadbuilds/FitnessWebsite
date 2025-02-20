c2569facabf62afda43c4d80312cc6d702f96a4b
Ahmad <crisitiano678@gmail.com>
2025-02-21 02:01:00 +0500

    First Page

---
+++ app/ui/NavBar.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { lustina } from "./font";
+import { usePathname } from "next/navigation";
+import clsx from 'clsx';
+import Image from 'next/image';
+import Link from 'next/link';
+export const NavBar = () => {
+    const DifferentId={
+        home:'/',
+        exercise:'/Exercises'
+      };
+      const pathname=usePathname();
+    return (
+    <div className="Home mt-8  lg:mt-4 w-full flex items-baseline content-center gap-44" >
+          <Image
+            src={'/Logo.png'}
+            width={55}
+            height={55}
+            alt="Biceps Image"
+          />
+          <div className="flex gap-10">
+            <Link className={clsx(
+              `${lustina.className} text-2xl `,
+              {
+                'border-b-4 border-red-500':pathname==DifferentId.home
+              }
+            )} href={'/'}>Home</Link>
+            <Link href={'/Exercises'} className={clsx(
+              `${lustina.className} text-2xl `,
+              {
+                'border-b-4 border-red-500':pathname==DifferentId.exercise
+              }
+            )}>Exercises</Link>
+          </div>
+        </div>
+  )
+}
+
+export default NavBar
---
+++ app/ui/font.ts
@@ -0,0 +1,3 @@
+import { Lusitana } from "next/font/google";
+
+export const lustina=Lusitana({subsets:['latin'],weight:['400','700']});
