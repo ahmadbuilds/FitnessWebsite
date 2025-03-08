1e2bf87e1cd1d1a845182df232428561fd53f715
Ahmad <crisitiano678@gmail.com>
2025-03-09 00:14:14 +0500

    Added Meta Data

--- app/Exercise/layout.tsx
+++ app/Exercise/layout.tsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import ExerciseNav from '../ui/ExerciseNav'
-
+import { Metadata } from 'next';
+export const metadata: Metadata = {
+  title: "Exercises",
+  description: "A fitness website just for showing the desired Workout",
+};
 const layout = ({children}:{children:React.ReactNode}) => {
   return (
     <div>
